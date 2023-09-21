8aa76a4d69dccbd33ab74c12b9152de39217208e
Martin-Lee-sla <104646916+Martin-Lee-sla@users.noreply.github.com>
2023-09-21 11:23:18 -0400

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,4 @@
-import { handlers } from "@/features/auth/auth-api";
+#import { handlers } from "@/features/auth/auth-api";
+import { handlers } from "./features/auth/auth-api";
 
 export { handlers as GET, handlers as POST };
